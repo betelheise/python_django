6065e3db7b1e6dbd0f82808b74adcf686485a92b
betelheise <sknov1828@gmail.com>
2026-03-29 17:08:22 +0000

    Added algs detail page, back button, view and URL

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from education import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('card/<int:pk>', views.card_detail, name='card_detail'),
 ]
--- education/views.py
+++ education/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Flashcard
 
 def home(request):
@@ -6,3 +6,8 @@ def home(request):
     cards = Flashcard.objects.all()
     # Send them to the template
     return render(request, 'education/home.html', {'cards': cards})
+
+def card_detail(request, pk):
+    # gets one card by it's id or gives error 404
+    card = get_object_or_404(Flashcard, pk=pk)
+    return render(request, 'education/card_detail.html', {'card': card})
